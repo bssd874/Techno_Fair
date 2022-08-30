3c861b845f422ceb10c59e1d5d5ce35cf352d285
bssd874 <81940296+bssd874@users.noreply.github.com>
2022-08-29 18:51:24 -0700

    commit4

--- TechnoFair/Assets/System/Teleport.cs
+++ TechnoFair/Assets/System/Teleport.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
     public Transform TeleportTarget;
+    public Transform TeleportStart;
     public CharacterController Player;
 
-    private void OnTriggerEnter(Collider other)
+    public void Teleportation1()
     {
         CharacterController cc = Player.GetComponent<CharacterController>();
         cc.enabled = false;
         Player.transform.position = TeleportTarget.transform.position;
+        cc.enabled = true;
+
+        Debug.Log("YES");
+    }
+
+    public void Teleportation2()
+    {
+        CharacterController cc = Player.GetComponent<CharacterController>();
+        cc.enabled = false;
+        Player.transform.position = TeleportStart.transform.position;
         cc.enabled = true;
 
         Debug.Log("YES");
